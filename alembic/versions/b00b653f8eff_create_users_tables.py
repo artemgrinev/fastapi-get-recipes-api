1a8fed1eb79b2c6d1ff11750d09e6c5ed58b7c23
GrinevArtem <artemgrinyov90@gmail.com>
2024-08-07 16:33:42 +0300

    add alembic migrations

---
+++ alembic/versions/b00b653f8eff_create_users_tables.py
@@ -0,0 +1,37 @@
+"""create users tables
+
+Revision ID: b00b653f8eff
+Revises: 
+Create Date: 2024-08-06 23:10:46.294787
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = 'b00b653f8eff'
+down_revision: Union[str, None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.create_table(
+        'users',
+        sa.Column('id', sa.Integer(), nullable=False),
+        sa.Column('email', sa.String(length=320), nullable=False),
+        sa.Column('hashed_password', sa.String(length=1024), nullable=False),
+        sa.Column('is_active', sa.Boolean(), nullable=False),
+        sa.Column('is_superuser', sa.Boolean(), nullable=False),
+        sa.Column('is_verified', sa.Boolean(), nullable=False),
+        sa.PrimaryKeyConstraint('id', name=op.f('pk_users'))
+        )
+    op.create_index(op.f('ix_users_email'), 'users', ['email'], unique=True)
+
+
+def downgrade() -> None:
+    op.drop_index(op.f('ix_users_email'), table_name='users')
+    op.drop_table('users')
