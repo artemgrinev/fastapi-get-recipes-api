b6a895b648e4c0a70a09bf1cf91069e189da67a4
GrinevArtem <artemgrinyov90@gmail.com>
2024-08-18 19:53:17 +0300

    add to init new models

--- models/__init__.py
+++ models/__init__.py
@@ -1,12 +1,26 @@
 __all__ = (
     "AccessToken",
     "Base",
+    "Comment",
+    "Product",
+    "ProductCategory",
     "Profile",
+    "Recipe",
+    "RecipesCategory",
+    "RecipeCategoryAssociation",
+    "RecipeProductAssociation",
     "User",
 )
 
 from .access_token import AccessToken
 from .base import Base
+from .comment import Comment
 from .profile import Profile
+from .product import Product
+from .product_category import ProductCategory
+from .recipes import Recipe
+from .recipes_category import RecipesCategory
+from .recipe_category_association import RecipeCategoryAssociation
+from .recipe_product_association import RecipeProductAssociation
 from .user import User
 
