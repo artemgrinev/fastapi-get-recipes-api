9d5b7c376d617980cf3c6573242570e1178f5de8
GrinevArtem <artemgrinyov90@gmail.com>
2024-08-18 16:26:16 +0300

    create recipes router, schemas

--- api/api_v1/routers/recipes.py
+++ api/api_v1/routers/recipes.py
@@ -1,10 +1,6 @@
 from fastapi import APIRouter
 
 from core.config import settings
-from core.schemas.recipes import (
-    Recipe,
-    RecipeResponse
-)
 
 router = APIRouter(
     prefix=settings.api.v1.recipes,
