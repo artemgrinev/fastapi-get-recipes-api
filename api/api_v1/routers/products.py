3660f4492220cdb39bba3df01b9e8a6f11632992
GrinevArtem <artemgrinyov90@gmail.com>
2024-08-18 16:24:57 +0300

    create product

--- api/api_v1/routers/products.py
+++ api/api_v1/routers/products.py
@@ -1,10 +1,6 @@
 from fastapi import APIRouter
 
 from core.config import settings
-from core.schemas.product import (
-    ProductRead,
-    ProductsResponse
-)
 
 router = APIRouter(
     prefix=settings.api.v1.product,
