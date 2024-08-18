8a2f4727df0a9871de130b25ee17d38b2ce64fa4
GrinevArtem <artemgrinyov90@gmail.com>
2024-08-18 16:13:45 +0300

    refactoring of models and interaction with the database

---
+++ models/__init__.py
@@ -0,0 +1,12 @@
+__all__ = (
+    "AccessToken",
+    "Base",
+    "Profile",
+    "User",
+)
+
+from .access_token import AccessToken
+from .base import Base
+from .profile import Profile
+from .user import User
+
