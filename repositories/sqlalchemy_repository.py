010387ae5a2dd0d85b94016fac04970af9c7cad0
GrinevArtem <artemgrinyov90@gmail.com>
2024-08-19 21:03:56 +0300

    FIX import

--- repositories/sqlalchemy_repository.py
+++ repositories/sqlalchemy_repository.py
@@ -4,7 +4,7 @@ from pydantic import BaseModel
 from sqlalchemy import delete, select, update
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from ..models.base import Base
+from models.base import Base
 from .base_repository import AbstractRepository
 
 
